d91fae047ce7f96e5d40fbc279449b016ff3b0b6
Ross Mawdsley <rossmawd@gmail.com>
2020-04-11 17:27:33 +0100

    switching back to final due to expectOne error

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,6 +1,7 @@
 import { CoursesService } from './courses.service';
 import { TestBed } from '@angular/core/testing';
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
+import { COURSES } from '../../../../server/db-data';
 
 describe('CoursesService', () => {
   let coursesService: CoursesService,
