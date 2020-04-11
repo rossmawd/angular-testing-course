726f786ee33c580e2524877b63bfdd6d15a81682
Ross Mawdsley <rossmawd@gmail.com>
2020-04-11 18:48:37 +0100

    more bug hunting

--- src/app/courses/services/courses.service.spec.ts
+++ src/app/courses/services/courses.service.spec.ts
@@ -1,41 +1,56 @@
 import { CoursesService } from './courses.service';
 import { TestBed } from '@angular/core/testing';
-import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing'
-import { COURSES } from '../../../../server/db-data';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing'
+import { COURSES, findLessonsForCourse } from '../../../../server/db-data';
+
 
 describe('CoursesService', () => {
   let coursesService: CoursesService,
-  HttpTestingController: HttpTestingController
+    httpTestingController: HttpTestingController
 
-  beforeEach( function() {
-   TestBed.configureTestingModule({
-     imports: [HttpClientTestingModule],
-     providers: [
-      CoursesService
-     ]
-   })
-   coursesService = TestBed.get(CoursesService)
-   HttpTestingController = TestBed.get(HttpTestingController);
+  beforeEach(function () {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        CoursesService
+      ]
+    })
+    coursesService = TestBed.get(CoursesService)
+    httpTestingController = TestBed.get(HttpTestingController);
   })
 
-  it('should retrieve all courses', ()=> {
-     coursesService.findAllCourses().subscribe(courses => {
-       expect(courses).toBeTruthy('No courses Returned')
+  it('should retrieve all courses', () => {
+    coursesService.findAllCourses().subscribe(courses => {
+      expect(courses).toBeTruthy('No courses Returned')
+      expect(courses.length).toBe(12, 'incorrect number of courses')
 
-       expect(courses.length).toBe(12, 'incorrect number of courses')
+      const course = courses.find(course => course.id == 12);
 
-       const course = courses.find(course => course.id == 12);
+      expect(course.titles.description).toBe(
+        'Angular Testing Course');
+    })
+    //intercepts the request
+    const req = httpTestingController.expectOne('/api/courses')
+    expect(req.request.method).toEqual("GET")
+    //makes a response that the subscribe block can use
+    req.flush({ payload: Object.values(COURSES) })
+  })
 
-       expect(course.titles.description).toBe(
-         'Angular Testing Course');
-     })
+  it('should find a course by ID', () => {
+    coursesService.findCourseById(12)
+      .subscribe(course => {
+        expect(course).toBeTruthy();
+        expect(course.id).toBe(12);
+      })
 
-     const req = HttpTestingController.expectOne('/api/courses')
-     expect(req.request.method).toEqual("GET")
+    const req = httpTestingController.expectOne('api/courses/12');
 
-     req.flush({payload: Object.values(COURSES)})
+    expect(req.request.method).toEqual("GET");
+    req.flush(COURSES[12]) //works as the keys in COURSES are numbers
+  })
 
+  afterEach(() => {
+    httpTestingController.verify()
   })
 }
-
 )
